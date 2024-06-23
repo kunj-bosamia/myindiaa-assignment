c2118e2660e1243f77dce44aec7c31d71a609388
kunjbosamiaFalkonry <kunj.bosamia@falkonry.com>
2024-06-23 20:10:45 +0530

    fix update password bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // Get user profile
@@ -13,11 +14,22 @@ exports.getUserProfile = async (req, res) => {
 // Update user profile
 exports.updateUserProfile = async (req, res) => {
   try {
+    // Exclude role from being updated
+    const updates = { ...req.body };
+    delete updates.role;
+
+    // Hash password if it is being updated
+    if (updates.password) {
+      const salt = await bcrypt.genSalt(10);
+      updates.password = await bcrypt.hash(updates.password, salt);
+    }
+
     const user = await User.findByIdAndUpdate(
       req.user.id,
-      { $set: req.body },
+      { $set: updates },
       { new: true, runValidators: true }
     ).select('-password');
+
     res.status(200).json({ success: true, data: user });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
