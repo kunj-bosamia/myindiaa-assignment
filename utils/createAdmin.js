5e7666bffc5a5955c5f8e5ee92559b3f7f8bdeae
kunjbosamiaFalkonry <kunj.bosamia@falkonry.com>
2024-06-24 22:14:56 +0530

    changes in create admin

--- utils/createAdmin.js
+++ utils/createAdmin.js
@@ -1,5 +1,4 @@
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
 
 // just for development purpose it will create an admin user if not exists whenever a server starts
 // email - admin@admin.com
@@ -12,13 +11,10 @@ exports.createAdmin = async () => {
     const existingAdmin = await User.findOne({ email: adminEmail });
 
     if (!existingAdmin) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(adminPassword, salt);
-
       const admin = new User({
         name: 'Admin',
         email: adminEmail,
-        password: hashedPassword,
+        password: adminPassword,
         role: 'admin',
       });
 
